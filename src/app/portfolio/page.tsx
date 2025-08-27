05abae7a5877f7507c5394efb4eebcc0368d7a47
dclarke35 <daniellemsurette@gmail.com>
2025-08-26 20:00:11 -0600

    feat: add pages

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,5 +1,4 @@
-
-export default function ContactPage() {
+export default function PortfolioPage() {
   return (
     <section className="flex flex-col mt-10 px-10 bg-var(--color-background) text-var(--color-foreground)">
       <h1 className="text-8xl font-bold mb-6">RanchStack CodeWorks</h1>
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,5 +1,5 @@
 
-export default function ContactPage() {
+export default function ServicesPage() {
   return (
     <section className="flex flex-col mt-10 px-10 bg-var(--color-background) text-var(--color-foreground)">
       <h1 className="text-8xl font-bold mb-6">RanchStack CodeWorks</h1>
